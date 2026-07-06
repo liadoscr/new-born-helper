6f95f7d18b5f54c8d4500cf223c59be2150c4268
liadoscr <liadoscr@users.noreply.github.com>
2026-07-06 07:49:20 +0300

    Configure Google sign-in client

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 window.LULLABY_LOG_CONFIG = {
   productionUrl: "https://neon-cajeta-6bc33d.netlify.app",
-  googleClientId: "",
+  googleClientId: "286863244298-spnqmcbtfs3t3rmm1a9t5skio4048loo.apps.googleusercontent.com",
   firebaseConfig: null,
   supabaseUrl: "",
   supabaseAnonKey: "",
