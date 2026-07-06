c2226072de31c171bb68f1860213c1ee8466f9ec
liadoscr <liadoscr@users.noreply.github.com>
2026-07-06 18:49:21 +0300

    Fix auth visibility and restore side icons

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.LULLABY_LOG_CONFIG = {
-  productionUrl: "https://neon-cajeta-6bc33d.netlify.app",
+  productionUrl: "https://new-born-helper.vercel.app",
   googleClientId: "286863244298-spnqmcbtfs3t3rmm1a9t5skio4048loo.apps.googleusercontent.com",
   firebaseConfig: null,
   supabaseUrl: "",
