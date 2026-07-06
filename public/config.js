46b2e2d5824246d01cb311db401443668fddc129
liadoscr <liadoscr@users.noreply.github.com>
2026-07-06 19:01:10 +0300

    Connect Firebase sync config

--- public/config.js
+++ public/config.js
@@ -1,7 +1,15 @@
 window.LULLABY_LOG_CONFIG = {
   productionUrl: "https://new-born-helper.vercel.app",
   googleClientId: "286863244298-spnqmcbtfs3t3rmm1a9t5skio4048loo.apps.googleusercontent.com",
-  firebaseConfig: null,
+  firebaseConfig: {
+    apiKey: "AIzaSyB5O0BnjRp7ZsW7qqK5aJQn4gMPCCbg-Lg",
+    authDomain: "newborn-helper-eb830.firebaseapp.com",
+    projectId: "newborn-helper-eb830",
+    storageBucket: "newborn-helper-eb830.firebasestorage.app",
+    messagingSenderId: "378396944885",
+    appId: "1:378396944885:web:a6557a8dbb3ce1dddf98bc",
+    measurementId: "G-TXNXP51K9K",
+  },
   supabaseUrl: "",
   supabaseAnonKey: "",
 };
--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "newborn-helper-v21";
+const CACHE_NAME = "newborn-helper-v22";
 const APP_SHELL = [
   "./",
   "./index.html",
